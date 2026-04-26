5a735b26a2f408949ea910fb5572de199906cc56
Hassnain Ali <61694409+hassnain-ali@users.noreply.github.com>
2026-04-26 18:34:10 +0500

    Refactor windows-install.sh for clarity and updates

--- windows-install.sh
+++ windows-install.sh
@@ -1,32 +1,41 @@
 #!/bin/bash
 set -e
 
-echo "[1/12] Updating system..."
-apt update -y && apt upgrade -y
+echo "[1/10] Updating package list..."
+apt update -y
 
-echo "[2/12] Installing dependencies..."
+echo "[2/10] Installing required packages..."
 apt install -y grub-pc-bin grub2-common parted gdisk wimtools ntfs-3g rsync wget curl
 
-echo "[3/12] Detecting main disk..."
+echo "[3/10] Detecting disk..."
 DISK=$(lsblk -dpno NAME | grep -E "/dev/sd|/dev/vd|/dev/nvme" | head -n1)
-echo "Detected disk: $DISK"
+echo "Using disk: $DISK"
 
 if [ -z "$DISK" ]; then
-  echo "No disk found. Exiting."
+  echo "❌ No disk found. Exiting."
   exit 1
 fi
 
-echo "[4/12] Getting disk size..."
+# Handle NVMe naming
+if [[ $DISK == *"nvme"* ]]; then
+  PART1=${DISK}p1
+  PART2=${DISK}p2
+else
+  PART1=${DISK}1
+  PART2=${DISK}2
+fi
+
+echo "[4/10] Getting disk size..."
 disk_size_mb=$(lsblk -b -dn -o SIZE $DISK)
 disk_size_mb=$((disk_size_mb / 1024 / 1024))
 
 part_size_mb=$((disk_size_mb / 2))
 
-echo "[5/12] Wiping disk..."
+echo "[5/10] Wiping disk..."
 wipefs -a $DISK
 sgdisk --zap-all $DISK
 
-echo "[6/12] Creating partitions..."
+echo "[6/10] Creating partitions..."
 parted -s $DISK mklabel msdos
 parted -s $DISK mkpart primary ntfs 1MiB ${part_size_mb}MiB
 parted -s $DISK mkpart primary ntfs ${part_size_mb}MiB 100%
@@ -34,27 +43,18 @@ parted -s $DISK mkpart primary ntfs ${part_size_mb}MiB 100%
 partprobe $DISK
 sleep 5
 
-PART1=${DISK}1
-PART2=${DISK}2
-
-# Fix NVMe naming
-if [[ $DISK == *"nvme"* ]]; then
-  PART1=${DISK}p1
-  PART2=${DISK}p2
-fi
-
-echo "[7/12] Formatting partitions..."
+echo "[7/10] Formatting partitions..."
 mkfs.ntfs -f $PART1
 mkfs.ntfs -f $PART2
 
-echo "[8/12] Mounting partitions..."
+echo "[8/10] Mounting partitions..."
 mkdir -p /mnt/win
 mkdir -p /mnt/install
 
 mount $PART1 /mnt/win
 mount $PART2 /mnt/install
 
-echo "[9/12] Installing GRUB..."
+echo "[9/10] Installing GRUB..."
 grub-install --boot-directory=/mnt/win/boot $DISK
 
 cat <<EOF > /mnt/win/boot/grub/grub.cfg
@@ -68,20 +68,21 @@ menuentry "Windows Installer" {
 }
 EOF
 
-echo "[10/12] Downloading Windows ISO..."
+echo "[10/10] Downloading Windows ISO..."
 cd /root
 
 wget -O windows.iso https://software-static.download.prss.microsoft.com/dbazure/26100.32230.260111-0550.lt_release_svc_refresh_SERVER_EVAL_x64FRE_en-us.iso
 
-echo "[11/12] Extracting Windows files..."
+echo "Mounting ISO..."
 mkdir -p /mnt/iso
 mount -o loop windows.iso /mnt/iso
 
+echo "Copying Windows files..."
 rsync -avh --progress /mnt/iso/ /mnt/win/
 
 umount /mnt/iso
 
-echo "[12/12] Downloading VirtIO drivers..."
+echo "Downloading VirtIO drivers..."
 wget -O virtio.iso https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/latest-virtio/virtio-win.iso
 
 mkdir -p /mnt/virtio
